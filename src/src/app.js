87d04333360805a82b2567c17d5f79506c60d9b7
Przemyslaw Sowinski <psowinski@users.noreply.github.com>
2022-12-22 22:43:41 +0100

    page view

--- src/src/app.js
+++ src/src/app.js
@@ -4,8 +4,8 @@ window.addEventListener('load', function() {
   const canvas = document.getElementById('app-canvas');
   const context = canvas.getContext('2d');
 
-  canvas.width = 550;
-  canvas.height = 500;
+  canvas.width = 650;
+  canvas.height = 600;
 
   const game = new Game(canvas);
 
